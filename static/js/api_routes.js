8e5f12e16ecc2aeb144c40b8c9fcbd8d9e848c66
msuzer <msuzer1@gmail.com>
2025-07-23 15:29:39 +0300

    centralized api_paths

---
+++ static/js/api_routes.js
@@ -0,0 +1,34 @@
+API_PATHS = {
+    "measurement": {
+        "start": "/gasera/api/measurement/start",
+        "abort": "/gasera/api/measurement/abort",
+        "state": "/gasera/api/measurement/state",
+    },
+    "connection": {
+        "status": "/gasera/api/connection_status"
+    },
+    "data": {
+        "server": "/gasera/api/data/server",
+        "dummy": "/gasera/api/data/dummy"
+    },
+    "settings": {
+        "read": "/gasera/api/settings/read",
+        "update": "/gasera/api/settings/update"
+    },
+    "dispatch": {
+        "instruction": "/gasera/api/dispatch/instruction"
+    },
+    "commandMap": {
+        "script": "/gasera/command_map.js"
+    },
+    "gpio": {
+        "control": "/gpio/api/gpio",
+        "motorStatus": "/gpio/motor/status",
+        "motorJog": "/gpio/motor/jog"
+    },
+    "system": {
+        "info": "/system/api/info",
+        "prefsGet": "/system/prefs",
+        "prefsPost": "/system/prefs"
+    }
+}
