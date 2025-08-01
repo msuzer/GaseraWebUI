cf5eb3ec0e7d009ff33c31e82fbc51c162528654
msuzer <msuzer1@gmail.com>
2025-08-01 13:42:35 +0300

    modified run and app scripts

--- run.py
+++ run.py
@@ -1,16 +1,7 @@
 # run.py
 
-import os
-from app import app, print_usage
+from app import app
+from waitress import serve
 
-print_usage()
-
-MODE = os.getenv("GASERA_MODE", "dev")
-
-if MODE == "prod":
-    from waitress import serve
-    print("[RUN] Serving via Waitress on http://0.0.0.0:5001")
-    serve(app, host='0.0.0.0', port=5001)
-else:
-    print("[RUN] Serving Flask dev server on http://127.0.0.1:5001")
-    app.run(host='127.0.0.1', port=5001)
+print("[RUN] Serving via Waitress on http://0.0.0.0:5001")
+serve(app, host='0.0.0.0', port=5001)
